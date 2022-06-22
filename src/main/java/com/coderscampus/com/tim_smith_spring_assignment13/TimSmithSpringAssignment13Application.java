8178c1f6613fb61d2b232f474ebcca6dcbd74a97
Timothy Alexander Smith <timothy.smith76@gmail.com>
2022-06-22 00:30:02 -0500

    Update TimSmithSpringAssignment13Application.java

--- src/main/java/com/coderscampus/com/tim_smith_spring_assignment13/TimSmithSpringAssignment13Application.java
+++ src/main/java/com/coderscampus/com/tim_smith_spring_assignment13/TimSmithSpringAssignment13Application.java
@@ -2,8 +2,10 @@ package com.coderscampus.com.tim_smith_spring_assignment13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class TimSmithSpringAssignment13Application {
 
 	public static void main(String[] args) {
